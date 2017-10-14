a14c7c4a8b050f9120d17ec2996aaf2b314499a1
ankorgh <inusahharis@gmail.com>
2017-10-14 14:01:21 +0000

    fixed

--- script.js
+++ script.js
@@ -1,78 +1,79 @@
-let board  = document.querySelector(".board")
-let spaces = document.querySelectorAll(".board div")
+let board  		    = document.querySelector(".board");
+let spaces 		    = document.querySelectorAll(".board div");
+let displayWon      = document.querySelector(".playerInfo");
+spaces				= Array.from(spaces);
+let humanPlayer     = "O";
+let machinePlayer   = "X"; 
+let winCombinations = [
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8],
+	[0,4,8],
+	[2,4,6]
+]
 
-board.addEventListener("click",function(e){
-	if(e.target.className === "space-one"){
-		if(!spaces[0].textContent.trim()){
-			spaces[0].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
+board.addEventListener("click",eventPlay,false);
 
-	if(e.target.className === "space-two"){
-		if(!spaces[1].textContent.trim()){
-			spaces[1].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
-	if(e.target.className === "space-three"){
-		if(!spaces[2].textContent.trim()){
-			spaces[2].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
-	if(e.target.className === "space-four"){
-		if(!spaces[3].textContent.trim()){
-			spaces[3].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
-	if(e.target.className === "space-five"){
-		if(!spaces[4].textContent.trim()){
-			spaces[4].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
-	if(e.target.className === "space-six"){
-		if(!spaces[5].textContent.trim()){
-			spaces[5].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
-	if(e.target.className === "space-seven"){
-		if(!spaces[6].textContent.trim()){
-			spaces[6].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
-	if(e.target.className === "space-eight"){
-		if(!spaces[6].textContent.trim()){
-			spaces[6].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
-	if(e.target.className === "space-nine"){
-		if(!spaces[7].textContent.trim()){
-			spaces[7].textContent = 0;
-			let num = generate();
-			spaces[num].textContent = 1;	
-		}
-	}
-})
+function eventPlay(e){
+	for(let i = 0; i < spaces.length; i++){
+		if(e.target.className === i.toString()){
+			if(!spaces[i].textContent.trim()){
+			 playerPlay(spaces,i,humanPlayer)
+			 if(!checkTie(spaces)){
+			 	playerPlay(spaces,bestSpot(),machinePlayer)
+			 } 
+			}
+		}		
+	}	
+}
 
-function generate(){
-	let num = Math.floor(Math.random() * 9);
-	// while(spaces[num].textContent.trim()){
-	// 	console.log(Boolean(spaces[num].textContent.trim()))
-	// 	num = Math.floor(Math.random() * 9);
-	// }
-	return num;
+
+function playerPlay(board,position,player){
+	board[position].textContent = player;
+	let playerWon =  checkPlayerWon(board,player);
+	if(playerWon){
+		gameOver(spaces,player);
+	}
+}
+
+function checkTie(board){
+	return emptySquares().length === 0
+}
+
+function bestSpot(){
+	emptySquares()[0].className
+	// console.log()
+}
+
+function emptySquares(){
+	let emptySpaces =  spaces.filter((elem) => {
+		return elem.textContent.trim() === ""
+	})
+	return emptySpaces;
+}
+
+function checkPlayerWon(board,player){
+	let playerWon = null;
+	let playerSpots = board.reduce((acc,elem,index) => {
+		if(elem.textContent == player){
+			return acc.concat(index)
+		}else{
+			return acc;
+		}
+	},[])
+	for(let [index,arr] of winCombinations.entries()){ 
+		if(arr.every((val) => playerSpots.includes(val))){
+			playerWon = {"index":index,"player":player}
+			break;
+		}
+	}
+	return playerWon;
+}
+
+function gameOver(board,player){
+	displayWon.style.display = "block";
+	displayWon.textContent = player + " Won";
 }
